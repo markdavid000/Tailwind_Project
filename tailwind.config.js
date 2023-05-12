6d1eefce0b2932482fbc54c813455083c1990de0
Mark-david <ojukwudavid59@outlook.com>
2023-05-12 11:13:10 +0100

    tailwind_challange

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ['*.{js,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        comf: ['Comfortaa', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
